4319ee22c6040f4336d3c2d95d1872e6ae840e69
Sleaker <sleaker@gmail.com>
2011-07-03 14:57:15 -0700

    moved getCType to Core Utility methods

--- src/net/milkbowl/combatevents/loot/CombatListener.java
+++ src/net/milkbowl/combatevents/loot/CombatListener.java
@@ -2,26 +2,12 @@ package net.milkbowl.combatevents.loot;
 
 import java.util.ArrayList;
 
-import org.bukkit.entity.Chicken;
-import org.bukkit.entity.Cow;
 import org.bukkit.entity.CreatureType;
-import org.bukkit.entity.Creeper;
-import org.bukkit.entity.Ghast;
-import org.bukkit.entity.Giant;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Pig;
-import org.bukkit.entity.PigZombie;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Sheep;
-import org.bukkit.entity.Skeleton;
-import org.bukkit.entity.Slime;
-import org.bukkit.entity.Spider;
-import org.bukkit.entity.Squid;
-import org.bukkit.entity.Wolf;
-import org.bukkit.entity.Zombie;
 
 import net.milkbowl.combatevents.CombatEventsListener;
 import net.milkbowl.combatevents.events.EntityKilledByEntityEvent;
+import net.milkbowl.combatevents.Utility;
 
 public class CombatListener extends CombatEventsListener{
 
@@ -30,7 +16,7 @@ public class CombatListener extends CombatEventsListener{
 
 	@Override
 	public void onEntityKilledByEntityEvent(EntityKilledByEntityEvent event) {
-		CreatureType cType = getCType(event.getKilled());
+		CreatureType cType = Utility.getCType(event.getKilled());
 		//If we gots a valid creature and a valid player lets do some drops!
 		if (event.getAttacker() instanceof Player && !cType.equals(null) ) {
 			String worldName = event.getAttacker().getWorld().getName();
@@ -45,38 +31,5 @@ public class CombatListener extends CombatEventsListener{
 			}
 		}
 
-	}
-
-	public CreatureType getCType(LivingEntity cEntity) {
-		if (cEntity instanceof Chicken)
-			return CreatureType.CHICKEN;
-		else if (cEntity instanceof Cow)
-			return CreatureType.COW;
-		else if (cEntity instanceof Creeper)
-			return CreatureType.CREEPER;
-		else if (cEntity instanceof Ghast)
-			return CreatureType.GHAST;
-		else if (cEntity instanceof Giant)
-			return CreatureType.GIANT;
-		else if (cEntity instanceof Pig)
-			return CreatureType.PIG;
-		else if (cEntity instanceof PigZombie)
-			return CreatureType.PIG_ZOMBIE;
-		else if (cEntity instanceof Sheep)
-			return CreatureType.SHEEP;
-		else if (cEntity instanceof Skeleton)
-			return CreatureType.SKELETON;
-		else if (cEntity instanceof Slime)
-			return CreatureType.SLIME;
-		else if (cEntity instanceof Spider)
-			return CreatureType.SPIDER;
-		else if (cEntity instanceof Squid)
-			return CreatureType.SQUID;
-		else if (cEntity instanceof Wolf)
-			return CreatureType.WOLF;
-		else if (cEntity instanceof Zombie)
-			return CreatureType.ZOMBIE;
-		else
-			return null;
 	}
 }
